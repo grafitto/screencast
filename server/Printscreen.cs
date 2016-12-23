f727a966fb0f6a39b53939752d765e7e9b2994c3
Kevin Nderitu <nderitukelvin19@gmail.com>
2016-12-23 20:31:37 +0300

    ScreenCast desktop process code in C#

---
+++ server/Printscreen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.IO;
+
+namespace printscreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(bitmap as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            MemoryStream memoryStream = new MemoryStream();
+            bitmap.Save(memoryStream, ImageFormat.Jpeg);
+            byte[] bitmapRecord = memoryStream.ToArray();
+            //bitmap.Save(@"D:\\screenshot.jpeg", ImageFormat.Jpeg);
+            Stream stdout = Console.OpenStandardOutput();
+            stdout.Write(bitmapRecord, 0, bitmapRecord.Length);
+            stdout.Flush();
+        }
+    }
+}
